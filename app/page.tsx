39c89f9e1ee98bbc992a97c200827a1b511e7322
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-02 21:26:17 +0100

    new main page + linear animation

--- app/page.tsx
+++ app/page.tsx
@@ -5,47 +5,32 @@ import GameCard from "./_components/GameCard";
 import Carousel from "./_components/GameCarousel";
 import { TrendingUpDown, Star } from "lucide-react";
 import Navbar from "./_components/Navbar";
+import Section from "./_components/Section";
+import RollingImages from "./_components/RollingImages";
+import CutePlayButton from "./_components/CutePlayButton";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
       <Navbar />
       <main className="flex min-h-screen w-full flex-col items-center justify-center pb-32 px-16 bg-white dark:bg-black">
-        <h1 className="text-3xl tracking-tight text-left w-full text-gray-900 dark:text-white sm:text-5xl">
-          <span className="font-bold text-7xl">Hello</span>, welcome to{" "}
-          <span className="font-bold text-7xxl">KatchUp</span>...
-        </h1>
-        <h2 className="mb-12 text-5xl text-center mt-16 w-full">
-          <span className="font-bold">Top</span> picks
-          <Star className="inline ml-2" />
-        </h2>
-        <div className="flex items-center w-full justify-center gap-20">
-          <GameCard
-            name="Flip Cards"
-            url="games/flip-cards"
-            img="flip_cards.png"
-            color="yellow"
-            description="hello"
-            feature="favorite"
-            featureColor="green"
-          />
-          <GameCard
-            name="One of Three"
-            url="games/one-of-three"
-            img="one_of_three.png"
-            color="red"
-            description="hello"
-            feature="popular"
-            featureColor="blue"
-          />
+        <div>
+          <Section>
+            <h2 className="text-4xl font-bold text-blue-500">Learn fast</h2>
+            <p className="text-blue-500 text-xl">
+              learn from our big database of actually{" "}
+              <span className="font-bold">useful</span> words
+            </p>
+          </Section>
+          <RollingImages />
+          <Section>
+            <h2 className="text-4xl font-bold ">Lets make it!</h2>
+            <p className="text-xl">One click and here we go!</p>
+
+            <CutePlayButton />
+          </Section>
         </div>
-        <div className="xl:w-[70%] border mt-20 border-white/[40%]" />
-        <h2 className="mb-6 text-5xl mt-40 text-center mr-[70%] w-full">
-          <span className="font-bold ">Explore</span> more
-          <TrendingUpDown className="inline ml-2" />
-        </h2>
-        <Carousel />
       </main>
     </div>
   );
