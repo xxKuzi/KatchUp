4b21313fd9c2cc63f5686baa753bdc4437b8be94
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-02 21:25:17 +0100

    new navbar + new logo

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -9,19 +9,25 @@ function Navbar() {
       <div className="z-10 fixed top-0 bg-black flex justify-between items-center border-b p-2 mt-2 hover:border-white transition duration-200 border-white/60 sm:min-w-[600px] rounded-xl w-[40%] hover-b-2">
         <div className="flex items-center justify-center">
           <button
-            className="px-4 py-2 cursor-pointer text-white/[80%] hover:text-white"
+            className="px-4 py-2 cursor-pointer text-white/80 hover:text-white"
             onClick={() => router.push("/")}
           >
-            <ArrowLeft className="inline mr-2" />
+            <img src={"/katch_up_logo.jpeg"} className="h-10" />
           </button>
           <button
-            className="px-4 py-2 cursor-pointer text-white/[80%] hover:text-white transition duration-200 rounded-lg"
-            onClick={() => router.push("/")}
+            className="px-4 py-2 cursor-pointer text-white/80 hover:text-white transition duration-200 rounded-lg"
+            onClick={() => router.push("/games")}
           >
-            Home
+            Games
           </button>
           <button
-            className="px-4 py-2 cursor-pointer text-white/[80%] hover:text-white"
+            className="px-4 py-2 cursor-pointer text-white/80 hover:text-white transition duration-200 rounded-lg"
+            onClick={() => router.push("/my-decks")}
+          >
+            My Decks
+          </button>
+          <button
+            className="px-4 py-2 cursor-pointer text-white/80 hover:text-white"
             onClick={() => router.push("/leaderboard")}
           >
             leaderboard {";)"}
